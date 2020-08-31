e4061432f94e296835a1b19541e3294c9903d0ea
munchiedunch <70247219+munchiedunch@users.noreply.github.com>
2020-08-31 11:30:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var prompter = prompt("what is your name"); 
+
+var blue = prompt("what is your favorite color");
+
+document.body.querySelector("#cool").innerHTML= "My name is" 
+
+document.body.querySelector("#lessCool").innerHTML=prompter
+
+document.body.querySelector("#red").innerHTML= "and my favorite color is"
+
+document.body.querySelector("#crayon").innerHTML=blue
